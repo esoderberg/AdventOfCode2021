4ffaf9c180fbf51788c4174047d6203964dac2b7
Erik Söderberg <ErikVSoderberg@gmail.com>
2021-12-06 17:02:12 +0100

    Completed Day 6, part 2. Solution to part 1 was way too slow. This one is almost instant. (Plus it solves part 1 as well)

--- Day6.fs
+++ Day6.fs
@@ -23,20 +23,42 @@ let rec SimulateDaysR currentDay maxDays (fishes: int list) (result: int list[])
     (result.[currentDay] <- fishes)
     if currentDay = maxDays then result
     else 
+        printfn "Now simulating day %d" (currentDay+1)
         SimulateDaysR (currentDay+1) maxDays (SimulateFishesDay fishes) result
 
 // Simulates the fishes for the given number of days
 let SimulateDays days (fishes: int list) = SimulateDaysR 0 days fishes (Array.create (days+1) List.empty<int>)
 
 
-let ExecutePart1 days (fishes: int list) = 
+let ExecutePart1 days (fishes: int list) shouldPrint= 
     let simulated = SimulateDays days fishes
-    Array.iteri (fun idx l -> printfn "After %d days: %s" idx (String.concat "," (List.map string l))) simulated
+    if shouldPrint then Array.iteri (fun idx l -> printfn "After %d days: %s" idx (String.concat "," (List.map string l))) simulated
     printfn "Day 6, Part 1: %d" (simulated.[simulated.Length-1].Length)
+
+let rec SimulateFishesAggregated currentDay days (fishesByDaysToBirth:int64[]) shouldPrint =
+    if currentDay = days then fishesByDaysToBirth
+    else 
+        let mothers = fishesByDaysToBirth.[0]
+        let newborns = mothers
+        for i in 0..7 do
+            fishesByDaysToBirth.[i] <- fishesByDaysToBirth.[i+1]
+        fishesByDaysToBirth.[8] <- newborns
+        fishesByDaysToBirth.[6] <- fishesByDaysToBirth.[6] + mothers
+        if shouldPrint then printfn "Day %d Fishes %d, Stages: %s" (currentDay+1) (Array.sum fishesByDaysToBirth) (String.concat "," (Array.map string fishesByDaysToBirth))
+        SimulateFishesAggregated (currentDay+1) days fishesByDaysToBirth shouldPrint
+
+
+let ExecutePartFast part days (fishes: int list) shouldPrint =
+    let fishesByDaysToBirth = Array.create 9 0L
+    let count = List.countBy id fishes
+    List.iter (fun (days,fishCount) -> (fishesByDaysToBirth.[days] <- int64 fishCount)) count
+    let result = SimulateFishesAggregated 0 days fishesByDaysToBirth shouldPrint
+    printfn "Day 6, Part %d: %d" part (Array.sum result)
 
 
 let Execute withFileInput = 
     let input = Seq.head (if withFileInput then GetInput 6 else TestInput)
-    let days = 80
     let fishes = List.ofSeq (Seq.map int (input.Split ','))
-    ExecutePart1 days fishes
+    ExecutePart1 80 fishes false 
+    ExecutePartFast 1 80 fishes false
+    ExecutePartFast 2 256 fishes false
