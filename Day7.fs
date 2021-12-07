a2957cf63b7f465261a575b1aecf81365e051f76
Erik Söderberg <ErikVSoderberg@gmail.com>
2021-12-07 20:20:12 +0100

    Completed day 7, part 1

---
+++ Day7.fs
@@ -0,0 +1,30 @@
+﻿module Day7
+open AoCFile
+
+let TestInput = seq {"16,1,2,0,4,2,7,1,2,14"}
+
+
+let GetMiddlePosition (sortedInput: int[]) = 
+    let halfLength = sortedInput.Length/2
+    if (sortedInput.Length % 2) = 0 then 
+       sortedInput.[halfLength]
+    else
+       (sortedInput.[halfLength] + sortedInput.[halfLength+1])/2
+
+let CalculateFuelSpentTo destination positions = 
+    let fuelExpenditures = Array.map (fun pos -> abs (destination - pos)) positions
+    let totalfuelExpenditure = Array.sum fuelExpenditures
+    totalfuelExpenditure
+
+let ExecutePart1 input = 
+    let sortedInput = Array.sort input
+    let middle = GetMiddlePosition sortedInput
+    let fuelSpent = CalculateFuelSpentTo middle sortedInput
+
+    printfn "Day 7, Part1: Fuel spent: %d, Middle: %d" fuelSpent middle
+    
+
+
+let Execute withRealInput = 
+    let input = Seq.toArray (Seq.map int ((Seq.head (if withRealInput then GetInput 7 else TestInput)).Split ","))
+    ExecutePart1 input
