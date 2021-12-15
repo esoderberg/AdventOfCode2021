28141c3e12952ff57803157b73eb8e96db17c4fa
Erik Söderberg <ErikVSoderberg@gmail.com>
2021-12-15 20:17:36 +0100

    Add solution to day 15, part 1

--- Day14.fs
+++ Day14.fs
@@ -22,19 +22,19 @@ let ExecutePart1 template pairMap =
     printfn "Day 14, Part 1: %d" ((snd max) - (snd min))
 
 
-let FastProcessTemplate (pairs: Dictionary<(char*char),int64>) (letterCount: Dictionary<char,int64>) (pairMap:IDictionary<(char*char),char>) =
-    let newPairs = Dictionary<(char*char),int64> []
-    let newLetterCount = Dictionary<char,int64> letterCount
+let FastProcessTemplate (pairs: Dictionary<(char*char),bigint>) (letterCount: Dictionary<char,bigint>) (pairMap:IDictionary<(char*char),char>) =
+    let newPairs = Dictionary<(char*char),bigint> []
+    let newLetterCount = Dictionary<char,bigint> letterCount
     Seq.iter (fun ((ch1,ch2), count) ->
         let insert = pairMap.[(ch1,ch2)]
         let p1 = (ch1, insert)
         let p2 = (insert, ch2)
-        let v1 = newPairs.GetValueOrDefault(p1,0L)
-        let v2 = newPairs.GetValueOrDefault(p2,0L)
-        if(count > 0L) then
+        let v1 = newPairs.GetValueOrDefault(p1,bigint.Zero)
+        let v2 = newPairs.GetValueOrDefault(p2,bigint.Zero)
+        if(count > bigint.Zero) then
             newPairs.[p1] <-  v1 + count
             newPairs.[p2] <-  v2 + count
-            newLetterCount.[insert] <- newLetterCount.GetValueOrDefault(insert, 0L) + count
+            newLetterCount.[insert] <- newLetterCount.GetValueOrDefault(insert, bigint.Zero) + count
             
         ) ([for entry in pairs do (entry.Key, entry.Value)])
 
@@ -42,22 +42,25 @@ let FastProcessTemplate (pairs: Dictionary<(char*char),int64>) (letterCount: Dic
 
 let ExecutePart2 (template:char list) (pairMap: IDictionary<(char * char),char>) =
     let startingPairs = Seq.toList (Seq.zip template (List.tail template))
-    let pairCounts = Dictionary<(char*char),int64> []
-    let letterCounts =  Dictionary<char,int64> []
+    let pairCounts = Dictionary<(char*char),bigint> []
+    let letterCounts =  Dictionary<char,bigint> []
 
-    List.iter  (fun pair -> pairCounts.[pair] <- pairCounts.GetValueOrDefault(pair,0L)+1L) (startingPairs)
-    List.iter (fun c -> letterCounts.[c] <- letterCounts.GetValueOrDefault(c,0L)+1L) template
+    List.iter  (fun pair -> pairCounts.[pair] <- pairCounts.GetValueOrDefault(pair,bigint.Zero)+bigint.One) (startingPairs)
+    List.iter (fun c -> letterCounts.[c] <- letterCounts.GetValueOrDefault(c,bigint.Zero)+bigint.One) template
 
-    let (resultPairs,resultLetters) = List.fold (fun (t,l) i -> FastProcessTemplate t l pairMap) (pairCounts, letterCounts) [1..40]
+    let (resultPairs,resultLetters) = List.fold (fun (t,l) i -> FastProcessTemplate t l pairMap) (pairCounts, letterCounts) [1..1000]
 
     let lettersList = [for entry in resultLetters do (entry.Key, entry.Value)]
     let max = List.maxBy (fun (chr,count) -> count) lettersList
     let min = List.minBy (fun (chr,count) -> count) lettersList
-    printfn "Day 14, Part 2: %d" ((snd max) - (snd min))
+    printfn "Day 14, Part 2: %s" (((snd max) - (snd min)).ToString())
 
 let Execute withRealInput = 
     let lines = Seq.toList (if withRealInput then GetInput 14 else GetTestInput 14)
     let (template, pairsRaw) = (List.ofSeq (List.head lines), List.tail (List.skip 1 lines))
     let pairMap = dict (List.map ParseInsertionPair pairsRaw)
+    let stopwatch = System.Diagnostics.Stopwatch.StartNew()
     ExecutePart1 template pairMap
     ExecutePart2 template pairMap
+    stopwatch.Stop()
+    printfn "Time: %dms" stopwatch.ElapsedMilliseconds
---
+++ Day15.fs
@@ -0,0 +1,54 @@
+﻿module Day15
+open AoCFile
+open GridUtil
+
+
+
+
+
+
+let FindShortestPath grid startCell endCell = 
+    let (mx,my) = GridUtil.GridBounds grid
+    let dist = [|for y in 0..my -> [|for x in 0..mx -> System.Int32.MaxValue|]|]
+    dist.[snd startCell].[fst startCell] <- 0
+    let prev = [|for y in 0..my -> [|for x in 0..mx -> (-1,-1)|]|]
+    let mutable Q = Set [|for y in 0..my do for x in 0..mx -> (x,y)|] 
+
+    // I just did not have it in me to try to do a non-mutable version 
+
+    while not Q.IsEmpty do
+        let (x, y) = Array.minBy (fun (x,y) -> dist.[y].[x]) (Array.ofSeq Q)
+        Q <- Set.remove (x,y) Q
+        for (u,v) in List.filter (fun cell -> Set.contains cell Q) (GridUtil.GetCellPlus grid (x,y)) do
+            let alt = dist.[y].[x] + grid.[v].[u]
+            if alt < dist.[v].[u] then
+                dist.[v].[u] <- alt
+                prev.[v].[u] <- (x,y)
+    
+    let mutable path = []
+    let mutable u = endCell
+    if prev.[snd u].[fst u] <> (-1,-1) || endCell = startCell then
+        while u <> (-1,-1) do
+           path <- u::path
+           u <- prev.[snd u].[fst u]
+
+    path
+
+
+    
+
+let ExecutePart1 grid =
+    let topLeft = (0,0)
+    let bottomRight = GridUtil.GridBounds grid
+    let shortestPath = FindShortestPath grid topLeft bottomRight
+    let risk = (List.sum (List.map (fun (x,y) -> grid.[y].[x]) shortestPath)) - (grid.[0].[0])
+    printfn "Day 15, Part 1: %d" risk
+
+    ()
+
+
+let Execute withRealInput = 
+    let lines = if withRealInput then GetInput 15 else GetTestInput 15
+    let grid = [|for line in lines -> Array.map (fun c -> (int c) - int '0') (Array.ofSeq line)|]
+    ExecutePart1 grid
+    ()
