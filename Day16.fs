7b84757e33b1c4c3a62e112c5d71947202ae28d1
Erik Söderberg <ErikVSoderberg@gmail.com>
2021-12-18 22:08:30 +0100

    Add solution to day 16, Part 2

--- Day16.fs
+++ Day16.fs
@@ -3,6 +3,17 @@
 open AoCFile
 open System
 
+type PacketType =
+    | SumOp = 0
+    | ProductOpt = 1
+    | MinOp = 2
+    | MaxOp = 3
+    | Literal = 4
+    | GtOp = 5
+    | LtOp = 6
+    | EqOp = 7
+
+
 type Bit = { Set:bool }
     with
     override this.ToString() = if this.Set then "1" else "0"
@@ -11,13 +22,12 @@ let (|Set|Unset|) (bit:Bit) = if bit.Set then Set else Unset
 
 let BitFrom (char: char) = { Set = (char = '1') }
 let BitsFrom (string: string) = List.map BitFrom (List.ofSeq string) 
-let Eval (bits : Bit list) = List.fold (fun acc bit -> (acc <<< 1) + (if bit.Set then 1 else 0)) 0 bits
-
+let Eval (bits : Bit list) = List.fold (fun acc bit -> (acc <<< 1) + (if bit.Set then 1L else 0L)) 0L bits
 
 
 type PacketHeader = {Version: int; TypeId: int}
 type Packet = OperatorPacket of OperatorPacket | LiteralValuePacket of LiteralValuePacket
-and LiteralValuePacket = {Header:PacketHeader; Value:int }
+and LiteralValuePacket = {Header:PacketHeader; Value:int64 }
 and OperatorPacket = {Header: PacketHeader; LengthTypeId:Bit; Packets: Packet list }
 
 let rec SumVersions packet = 
@@ -71,12 +81,12 @@ and ParseOperatorPacket header (bits: Bit list) : (OperatorPacket * Bit list) =
     let packets, bits = 
         match length.[0] with
         | Set -> 
-            let subPacketCount = Eval (List.take 11 bits) 
+            let subPacketCount = int (Eval (List.take 11 bits))
             let bits = List.skip 11 bits
             ParseNumPackets subPacketCount bits
         | Unset -> 
             let subPacketBits = Eval (List.take 15 bits)
-            let bits, remaining = List.splitAt subPacketBits (List.skip 15 bits)
+            let bits, remaining = List.splitAt (int subPacketBits) (List.skip 15 bits)
             (ParsePackets bits, remaining)
 
     ( {Header = header; LengthTypeId = length.[0]; Packets = packets}, bits)
@@ -109,19 +119,31 @@ and ParseNumPackets count bits =
     (packet::(packets),bits)
 
 
-
+let rec Evaluate packet = 
+    match packet with 
+    | LiteralValuePacket v -> int64 v.Value
+    | OperatorPacket p -> 
+        match LanguagePrimitives.EnumOfValue p.Header.TypeId with
+        | PacketType.SumOp -> List.sumBy Evaluate p.Packets
+        | PacketType.ProductOpt -> List.fold (*) 1L (List.map Evaluate p.Packets)
+        | PacketType.MinOp -> (List.min (List.map Evaluate p.Packets))
+        | PacketType.MaxOp -> (List.max (List.map Evaluate p.Packets))
+        | PacketType.GtOp -> if (Evaluate p.Packets.[0]) > (Evaluate p.Packets.[1]) then 1L else 0L
+        | PacketType.LtOp -> if (Evaluate p.Packets.[0]) < (Evaluate p.Packets.[1]) then 1L else 0L
+        | PacketType.EqOp -> if (Evaluate p.Packets.[0]) = (Evaluate p.Packets.[1]) then 1L else 0L
+        | _-> raise (invalidArg "Packet" "Invalid header type")
 
 
 let TestInput1 = "D2FE28"
-let Test1Result = {Header={Version=6; TypeId=4}; Value=2021}
+let Test1Result = {Header={Version=6; TypeId=4}; Value=2021L}
 
 let TestInput2 = "38006F45291200"
 let Test2Result = {
     Header={Version=1; TypeId=6};
     LengthTypeId= {Set=false}
     Packets = [
-        LiteralValuePacket {Header={Version=6; TypeId=4}; Value=10};
-        LiteralValuePacket {Header={Version=2;TypeId=4}; Value=20}
+        LiteralValuePacket {Header={Version=6; TypeId=4}; Value=10L};
+        LiteralValuePacket {Header={Version=2;TypeId=4}; Value=20L}
         ]}
 
 
@@ -133,6 +155,10 @@ let TestInput6 = "A0016C880162017C3686B18A3D4780"
 let CheckVersionSum expected input = 
     let packet,_ = (ParsePacket (BitsFromHexes (List.ofSeq input)))
     if expected = SumVersions packet then "Pass" else "Fail"
+
+let CheckEvaluated expected input = 
+    let packet,_ = (ParsePacket (BitsFromHexes (List.ofSeq input)))
+    if expected = Evaluate packet then "Pass" else "Fail"
 
 let ExecuteTests = 
     let bits = BitsFromHexes TestInput1
@@ -152,10 +178,24 @@ let ExecuteTests =
     printfn "Test 4: %s" (CheckVersionSum 12 TestInput4)
     printfn "Test 5: %s" (CheckVersionSum 23 TestInput5)
     printfn "Test 6: %s" (CheckVersionSum 31 TestInput6)
+    printfn "Test 7: %s" (CheckVersionSum 14 "EE00D40C823060")
+
+    printfn "Test P2 1: %s" (CheckEvaluated 3L "C200B40A82")
+    printfn "Test P2 2: %s" (CheckEvaluated 54L "04005AC33890")
+    printfn "Test P2 3: %s" (CheckEvaluated 7L "880086C3E88112")
+    printfn "Test P2 4: %s" (CheckEvaluated 9L "CE00C43D881120")
+    printfn "Test P2 5: %s" (CheckEvaluated 1L "D8005AC2A8F0")
+    printfn "Test P2 6: %s" (CheckEvaluated 0L "F600BC2D8F")
+    printfn "Test P2 7: %s" (CheckEvaluated 0L "9C005AC2F8F0")
+    printfn "Test P2 8: %s" (CheckEvaluated 1L "9C0141080250320F1802104A08")
     ()
 
 
-
+let ExecutePart2 lines =
+    let bits = List.collect (fun line -> List.collect (fun c -> BitsFromHex c) (List.ofSeq line)) lines
+    let packet, bits = ParsePacket bits
+    printfn "Day 16, Part 2: %d" (Evaluate packet)
+    ()
 
 let ExecutePart1 lines = 
     let bits = List.collect (fun line -> List.collect (fun c -> BitsFromHex c) (List.ofSeq line)) lines
@@ -168,4 +208,5 @@ let Execute withRealInput =
     else
     let lines = List.ofSeq (GetInput 16)
     ExecutePart1 lines
+    ExecutePart2 lines
     ()
